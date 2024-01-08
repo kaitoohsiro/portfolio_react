bc9eecab7b785a279a51c70e0f70526b09227a83
kaitoohsiro <ooshirokaito@ooshirokaitonoMac-mini.local>
2024-01-08 12:38:55 +0900

    first commit

--- src/components/parts/AboutBlock.tsx
+++ src/components/parts/AboutBlock.tsx
@@ -6,7 +6,7 @@ const AboutBlock = () => {
         <div className="profile-container">
             <img src={Profile} alt="Profile Image" className="profile-image" />
             <div className="profile-info">
-                <p>名前：　大城　海斗</p>
+                <p>名前：　大城 海斗</p>
                 <p>年齢：　25</p>
                 <p>GitHub：　<a href="https://github.com/kaitoohsiro" target="_blank" rel="noopener noreferrer">github</a></p>
                 <p>Qiita：　<a href="https://qiita.com/kaito_111" target="_blank" rel="noopener noreferrer">qiita</a></p>
